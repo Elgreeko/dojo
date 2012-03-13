28a0b711a12c135dba2093b4c71f6a5c56eba7fc
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2012-03-13 19:32:13 -0300

    Refactor the code to use lambdas

--- dojo-20120313/lib/anagram.ex
+++ dojo-20120313/lib/anagram.ex
@@ -1,13 +1,9 @@
 defmodule Anagram do
-  def anagram_of(word, [word | _tail]) do
-    [word]
-  end
+  def anagram_of(word, [word | _tail]), do: [word]
 
   def anagram_of(word, [_head | tail]) do
     anagram_of(word, tail)
   end
 
-  def anagram_of(_word, _list) do
-    []
-  end
+  def anagram_of(_word, _list), do: []
 end
