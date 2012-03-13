61694ef8b1c6d2f5c114dbb0895d132024993376
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2012-03-13 20:26:53 -0300

    Refactoring to use the lang features

--- dojo-20120313/lib/anagram.ex
+++ dojo-20120313/lib/anagram.ex
@@ -1,10 +1,6 @@
 defmodule Anagram do
   def anagram_of(word, list) do
-    Enum.filter(list, is_anagram(word, &1))
-  end
-
-  defp is_anagram(word, candidate) do
-    sort(word) == sort(candidate)
+    Enum.filter(list, fn(c, do: sort(word) == sort(c)))
   end
 
   defp sort(word) do
