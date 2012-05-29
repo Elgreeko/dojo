263d32d3312f9c37dfc7e91ba4702ec092fb2897
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2012-05-29 20:23:27 -0300

    Refactoring

--- dojo-20120529/lib/hello_kitty.rb
+++ dojo-20120529/lib/hello_kitty.rb
@@ -1,17 +1,17 @@
 class HelloKitty
   def initialize(word, count)
-    @word = ''
     @length = word.length
-    count.times do
-      @word += word
-    end
+    @word = word*count
+  end
+
+  def shift(i)
+    @word[i..-1] + @word[0...i]
   end
 
   def write
     output = ""
     @length.times do |i|
-      new_word = @word[i..-1] + @word[0...i]
-      output += new_word + "\n"
+      output += shift(i) + "\n"
     end
     output.chomp
   end
